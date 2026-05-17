aef99096d93471c8d9917dcbb60c5858f8959fd2
AlexisJAP-28 <alexisxavier.2806@gmail.com>
2026-05-17 14:29:23 -0500

    Add files via upload

---
+++ PYTHON 2DO NIVEL- EVALUACION PRACTICA - ALEXIS AÑAMISE/SUMA_NUMEROS.py
@@ -0,0 +1,17 @@
+print ("ALGORITMOS Y FUNDAMENTOS DE PROGRAMACIÓN")
+print ("EVALUACIÓN PRÁCTICA 2")
+print ("ALexis Añamise")
+print ("EJERCICIO 2")
+print ("Analice el siguiente código y explique qué hace. Luego, ejecútelo y muestre el resultado:")
+print ("")
+print ("SUMA DE NÚMEROS")
+#PROCESO
+def suma_numeros(n):
+    suma = 0
+    for i in range(1, n + 1):
+        suma += i
+    return suma
+print(suma_numeros(5))
+print("")
+print ("FINALIZACIÓN DEL EJERCICIO")
+#FINALIZACIÓN DE LA EVALUACIÓN
